5120c84c6713f50a488a8ecc584f4382c5db4390
Jayjeet Kumar <80195355+Jayjeet1013@users.noreply.github.com>
2023-11-30 22:28:39 +0530

    Add files via upload

---
+++ data/contributors.js
@@ -0,0 +1,30 @@
+export const contributorsData = [
+     {
+        name: "Jayjeet Kumar",
+        title: "WEB3 Developer",
+        profile_url: "/profiles/jayjeet.png",
+        twitter: "",
+        linkedin: "",
+        discord: "",
+    },
+    
+    {
+        name: "Aditya Raj",
+        title: "Frontend Developer",
+        profile_url: "/profiles/aditya.png",
+        twitter: "",
+        linkedin: "",
+        discord: "",
+    },
+
+    {
+        name: "Aman",
+        title: "Project Manager",
+        profile_url: "/profiles/aman.png",
+        twitter: "",
+        linkedin: "",
+        discord: "",
+    }
+    
+   
+]
