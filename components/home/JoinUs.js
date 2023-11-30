644994b6e8c6bb99ea9654c1c0a5a407cf177562
Jayjeet1013 <jayjeet1013@gmail.com>
2023-12-01 01:36:08 +0530

    update

--- components/home/JoinUs.js
+++ components/home/JoinUs.js
@@ -63,7 +63,7 @@ export default function JoinUs() {
   return (
     <div className="text-center bg-[#FECF21] border-dark border-t-2">
       <div className="section">
-        <h3 className="mb-5 h1">Ready to get into the WEB3.0 ?</h3>
+        <h3 className="mb-5 h1"> Subscribe to our Newsletter!</h3>
        
       <Box   >
 <Center>
