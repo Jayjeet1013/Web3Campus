394ba0706da5641ea019ed1e491e5d0bba29a4c3
Jayjeet1013 <jayjeet1013@gmail.com>
2023-12-01 05:42:09 +0530

    update

--- components/home/Header.js
+++ components/home/Header.js
@@ -2,15 +2,16 @@ import Link from "next/link";
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import Nav from "./Nav";
+import { Box, Center } from "@chakra-ui/react";
 
 function Earn() {
   return (
     <div className="w-full">
       <div className="w-full p-5 border-b-2 border-dark">
         <h1 className="h3 mb-5">
-          Browse <br />
+          Browse 
           <b>29,375</b> blockchain jobs in web3
-          <br /> at <b>2,936 projects.</b>
+           at <b>2,936 projects.</b>
         </h1>
         <Link href="https://web3.career/">
           <button className="btn">💰 Explore now</button>
@@ -20,8 +21,8 @@ function Earn() {
       <div className="w-full p-5">
         <h1 className="h3 mb-5">
           Solve blockchain
-          <br /> related bounties
-          <br /> and earn <b>Rewards</b>
+         related bounties
+         and earn <b>Rewards</b>
         </h1>
 
         <button className="btn">💰 Solve now</button>
@@ -37,8 +38,8 @@ function Build() {
       <div>
         <h3 className="mb-5 h3 leading-[50px]">
           We have prepared a list of
-          <br /> <b> 100+ </b> Blockchain projects,
-          <br /> that you can <b>Build </b> right now
+        <b> 100+ </b> Blockchain projects,
+       that you can <b>Build </b> right now
         </h3>
 
         <Link href="/resources/projects">
@@ -52,11 +53,12 @@ function Build() {
 
 function Learn() {
   return (
-    <div>
+    <Box>
+      
       <div className="w-full p-5 border-b-2 border-dark">
         <h1 className="h3 mb-5">
           Gain More Knowledge
-          <br />
+        
           <b>By Reading</b> Whitepapers <br />
         </h1>
         <Link href="/resources/whitepaper">
@@ -73,29 +75,33 @@ function Learn() {
           <button className="btn">👨‍💻 Learn now</button>
         </Link>
       </div>
-    </div>
+    </Box>
   );
 }
 
 export default function Header() {
   return (
-    <div>
-      <div className="bg-[#F5F6FF] min-h-[80vh]">
+    <Box>
+      
+            <div className="bg-[#F5F6FF]">
         <Row className="m-0">
-          <Col sm={4} className="p-0 h-[80vh] bg-[#D7FFEE] flex items-center">
+          {/* Learn Section */}
+          <Col sm={12} md={4} className="p-0 h-[80vh] bg-[#D7FFEE] flex items-center">
             <Learn />
           </Col>
-          <Col
-            sm={4}
-            className="p-0 h-[80vh] border-r-2 border-l-2 border-dark bg-[#EAE7FF]"
-          >
+          
+          {/* Build Section */}
+          <Col sm={12} md={4} className="p-0 h-[80vh] border-r-2 border-l-2 border-dark bg-[#EAE7FF]">
             <Build />
           </Col>
-          <Col sm={4} className="p-0 h-[80vh] bg-[#F3FEDE] flex items-center">
+          
+          {/* Earn Section */}
+          <Col sm={12} md={4} className="p-0 h-[80vh] bg-[#F3FEDE] flex items-center">
             <Earn />
           </Col>
         </Row>
       </div>
-    </div>
+
+    </Box>
   );
 }
